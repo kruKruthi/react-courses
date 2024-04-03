83b14262a064da1a419de6af18c434a57f6a7e58
kruthika <kruthi293@gmail.com>
2024-04-03 10:48:09 +0530

    Added content allignment changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,25 +1,25 @@
 import TeamCards from "./TeamCards";
-import { useState } from "react";
 import teamsData from "../utils/mockData";
-
+import { MEMBER_TITLE, ADMIN_TITLE } from "../utils/constant";
 
 const Body = () => {
-  const [listOfUsers, setListOfUsers] = useState(teamsData);
-  // const [filteredList, setFilteredUser] = useState([]);
-
-  let memberData = listOfUsers.filter((user) => user.role === "member");
-  let adminData = listOfUsers.filter((user) => user.role === "admin");
-  // setFilteredUser(memberData);
+  let memberData = teamsData.filter((user) => user.role === "member");
+  let adminData = teamsData.filter((user) => user.role === "admin");
 
   return (
-  <div className="body-layout">
-    <h1>Administrators</h1>
-    <div className="admin-card">
-      {adminData.map((user) => <TeamCards data={user} />)}
+  <div>
+    <div className="admin">
+      <h1 className="section-layout">{ADMIN_TITLE}</h1>
+      <div className="admin-card">
+        {adminData.map((user) => <TeamCards data={user} />)}
+      </div>
     </div>
-    <h1>Members</h1>
-    <div className="member-card">
-      {memberData.map((user) => <TeamCards data={user} />)}
+    <hr className="hr-line"/>
+    <div className="member">
+      <h1 className="section-layout">{MEMBER_TITLE}</h1>
+      <div className="member-card">
+        {memberData.map((user) => <TeamCards data={user} />)}
+      </div>
     </div>
   </div>
   );
--- src/components/TeamCards.jsx
+++ src/components/TeamCards.jsx
@@ -1,12 +1,9 @@
-import { CDN_URL } from "../utils/constant";
-
 const TeamCards = (props) => {
   const { first_name, last_name, img, email} = props?.data;
   return (
     <div className="res-card">
-      <img className= "res-logo" src={img} />
-      <h3>{first_name +" " + last_name }</h3>
-      <h5>{email}</h5>
+      <img className= "image-content" src={img} />
+      <h3 className="name-content">{first_name +" " + last_name } <br /> <h6 className="email-content"> {email}</h6></h3>
     </div>
   )
 };
